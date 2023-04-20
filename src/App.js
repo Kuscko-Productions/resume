394fec244e851e242235d6e5c1171a8ab37da830
Kuscko <patrick.kellyse@gmail.com>
2023-04-20 00:19:38 -0400

    Updated header tags. Updated bullet points with finer details.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className='row'>
           <div id='name' className='col-sm-6 col-md-6 col-lg-7'>
             <h1>Patrick Kelly</h1>
-            <h2>Software Developer</h2>
+            <h3>Software Developer</h3>
           </div>
           <div className='col-sm-6 col-md-6 col-lg-5'>
             <h3>Contact</h3>
@@ -30,65 +30,66 @@ function App() {
 
       <div className='row'>
         <div id='about' className='col-sm-9 col-md-9 col-lg-9'>
-          <h2>About Me</h2>
-          <p>Dynamic, professionally educated, and multifaceted software engineer. Capable of designing and developing secure web applications.</p>
-          <p>While ensuring the best possible performance, quality, and responsiveness of applications. Able to evolve to meet shifting needs 
-            and priorities. With a passion to build and maintain applications for long-term use.</p>
+          <h3>About Me</h3>
+          <p>Experienced software engineer with a passion for designing and developing secure web applications, 
+            ensuring high performance, quality, and responsiveness. Adaptable to changing needs and committed to 
+            building and maintaining applications for long-term use.</p>
         </div>
         <div id='education' className='col-sm-3 col-md-3 col-lg-3'>
-          <h2>Education</h2>
+          <h3>Education</h3>
           <p>Pasco-Hernando State College: Associates in Science - Computer Programming and Analysis</p>
         </div>
       </div>
 
       <div className='row'>
         <div id='experience'>
-          <h2>Experience</h2>
+          <h3>Experience</h3>
           <div className='experience-details'>
             <h4>Automation Engineer</h4>
             <h5><a href='https://www.proforma.com'>Proforma</a> / Tampa, FL / November 2021 - present</h5>
             <ul>
-              <li>Analyze requirements, design, and develop testing suites and an automation framework using the AGILE methodology and TDD approach.</li>
-              <li>Develop and maintain automated tests using C# and Selenium.</li>
-              <li>Prepare and maintain documentation for writen test cases.</li>
-              <li>Perform manual testing as needed.</li>
-              <li>Maintain and improve End-to-End controllers and services in the API.</li>
+              <li>Prepared and maintained documentation for written test cases to ensure comprehensive test coverage and effective communication within the team.</li>
+              <li>Spearheaded the implementation of a new real-time test reporting system, resulting in increased productivity and faster issue resolution.</li>
+              <li>Developed and applied a comprehensive QA process that significantly improved product quality and customer satisfaction.</li>
+              <li>Developed and maintained automated tests using C# and Selenium to ensure consistent and efficient testing.</li>
+              <li>Mentored and provided technical guidance to junior developers to improve overall team performance.</li>
               <li>Created an Automation Framework using MSTest, and other iterations with NUnit and XUnit packages.</li>
-              <li>Improved development speed and test reliabilty by ease of use up to 65% since 2021.</li>
-              <li>Reduced runtime of all test suites by 50% and required resources by 44%.</li>
             </ul>
           </div>
+          <div className='pagebreak'/>
           <div className='experience-details'>
             <h4>Web Developer</h4>
             <h5><a href='https://lostsouls.org/'>Telesmatic Systems LLC</a> / Port Richey FL  / November 2022 - Present</h5>
             <ul>
-              <li>Assist in the development of authoring software on a full stack capacity utlizing web software such as React Js and Django.</li>
-              <li>Designed complex data structures using the Django ORM and a MySql database.</li>
-              <li>Designed and configured CI/CD piplines in GitLab.</li>
+              <li>Collaborated in the full-stack development of authoring software using React Js and Django web frameworks.</li>
+              <li>Engineered and implemented intricate data structures utilizing the Django ORM and a MySql database.</li>
+              <li>Designed and set up continuous integration and delivery (CI/CD) pipelines utilizing GitLab for improved software development workflow.</li>
             </ul>
           </div>
           <div className='experience-details'>
             <h4>Computer Programmer</h4>
             <h5>Johnny on the Spot - Furniture Repair / Springhill, FL / September 2020 - September 2021</h5>
             <ul>
-              <li>Planned, Designed, Developed, and Maintained a contract management system that allowed this company to increase the outreach of its employees to individual clients.</li>
-              <li>Created a tool to get the company's capability statement distributed.</li>
-              <li>The CMS had a route customizer for day-to-day jobs based on employee locations.</li>
+              <li>Led the development of a new contract management system resulting in a 50% increase in efficiency and a 20% reduction in costs.</li>
+              <li>Troubleshot and resolved defects in existing software applications, resulting in improved system stability and reliability.</li>
+              <li>Designed and implemented new data models and algorithms resulting in improved performance and scalability.</li>
             </ul>
           </div>
           <div className='experience-details'>
             <h4>Freelance Developer</h4>
             <h5>Kuscko Productions / Remote / June 2015 - August 2020</h5>
             <ul>
-              <li>Provided custom scripting services, web application development and maintenance.</li>
-              <li>Created a resource management system that reduced any downtime an application was running on a server.</li>
+              <li>Collaborated with clients to understand their business needs and requirements, delivering solutions that met or exceeded expectations.</li>
+              <li>Developed and maintained multiple custom web applications resulting in increased customer satisfaction and repeat business.</li>
+              <li>Worked independently and managed projects from conception to delivery, ensuring timely and successful outcomes.</li>
+              <li>Utilized best practices in software development to create scalable and maintainable code.</li>
             </ul>
           </div>
         </div>
       </div>
       <div className='row'>
         <div id='skills'>
-          <h2>Skills</h2>
+          <h3>Skills</h3>
           <div id='skill-list'>
             <div className='d-flex justify-content-start'>
               <ul>
