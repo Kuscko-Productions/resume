8c52fe7b8c28ae64a2bd490d4dfe4610f4e1c655
Kuscko <patrick.kellyse@gmail.com>
2023-04-19 20:02:44 -0400

    updated title. updated headers. updated wireframe for content positioning. added some .css for styling.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { faGitSquare } from '@fortawesome/free-brands-svg-icons';
 import './App.scss';
 
 function App() {
+  document.title = 'Patrick\'s Resume';
   return (
     <ThemeProvider
       breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
       minBreakpoint="xxs"
     >
       <div className='container'>
-        <div id='info' className='container'>
+        <div id='info'>
           <div className='row'>
-            <div className='col-sm-6 col-md-6 col-lg-7'>
+            <div id='name' className='col-sm-6 col-md-6 col-lg-7'>
               <h1>Patrick Kelly</h1>
               <h2>Software Developer</h2>
             </div>
             <div className='col-sm-6 col-md-6 col-lg-5'>
-              <h2>Contact</h2>
+              <h3>Contact</h3>
               <ListGroup variant='flush'>
                 <ListGroupItem><FontAwesomeIcon icon={faPhone} size='lg'/> <a href='tel:+17273644111'>(727) 364-4111</a></ListGroupItem>
                 <ListGroupItem><FontAwesomeIcon icon={faGlobe} size='lg'/> <a href='https://www.google.com/maps/place/Tampa+Palms,+Tampa,+FL/@28.0481853,-82.4365068,12z/data=!4m6!3m5!1s0x88c2b7f72210b7e5:0x61e9582cd056b9b2!8m2!3d28.0976718!4d-82.3985051!16s%2Fm%2F026nm4z'>Tampa Palms, Florida, USA</a></ListGroupItem>
@@ -29,24 +30,26 @@ function App() {
             </div>
           </div>
         </div>
-        
-        <div id='about' className='row'>
-          <h2>About Me</h2>
-          <p>Dynamic, professionally educated, and multifaceted software engineer. Capable of designing and developing secure web applications.</p>
-          <p>While ensuring the best possible performance, quality, and responsiveness of applications. Able to evolve to meet shifting needs 
-            and priorities. With a passion to build and maintain applications for long-term use.</p>
-          <br></br>
-        </div>
-
-        
 
         <div className='row'>
-          <div id='experience' className='col-sm-6 col-md-6 col-lg-9'>
+          <div id='about' className='col-sm-9 col-md-9 col-lg-9'>
+            <h2>About Me</h2>
+            <p>Dynamic, professionally educated, and multifaceted software engineer. Capable of designing and developing secure web applications.</p>
+            <p>While ensuring the best possible performance, quality, and responsiveness of applications. Able to evolve to meet shifting needs 
+              and priorities. With a passion to build and maintain applications for long-term use.</p>
+          </div>
+          <div id='education' className='col-sm-3 col-md-3 col-lg-3'>
+            <h2>Education</h2>
+            <p>Pasco-Hernando State College: Associates in Science - Computer Programming and Analysis</p>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div id='experience'>
             <h2>Experience</h2>
-            <br></br>
-            <div className='experience'>
-              <h3>Automation Engineer</h3>
-              <h4><a href='https://www.proforma.com'>Proforma</a> / Tampa, FL / November 2021 - present</h4>
+            <div className='experience-details'>
+              <h4>Automation Engineer</h4>
+              <h5><a href='https://www.proforma.com'>Proforma</a> / Tampa, FL / November 2021 - present</h5>
               <ul>
                 <li>Analyze requirements, design, and develop testing suites and an automation framework using the AGILE methodology and TDD approach.</li>
                 <li>Develop and maintain automated tests using C# and Selenium.</li>
@@ -58,66 +61,71 @@ function App() {
                 <li>Reduced runtime of all test suites by 50% and required resources by 44%.</li>
               </ul>
             </div>
-            <br></br>
-            <div className='experience'>
-              <h3>Web Developer</h3>
-              <h4><a href='https://lostsouls.org/'>Telesmatic Systems LLC</a> / Port Richey FL  / November 2022 - Present</h4>
+            <div className='experience-details'>
+              <h4>Web Developer</h4>
+              <h5><a href='https://lostsouls.org/'>Telesmatic Systems LLC</a> / Port Richey FL  / November 2022 - Present</h5>
               <ul>
                 <li>Assist in the development of authoring software on a full stack capacity utlizing web software such as React Js and Django.</li>
                 <li>Designed complex data structures using the Django ORM and a MySql database.</li>
                 <li>Designed and configured CI/CD piplines in GitLab.</li>
               </ul>
             </div>
-            <br></br>
-            <div className='experience'>
-              <h3>Computer Programmer</h3>
-              <h4>Johnny on the Spot - Furniture Repair / Springhill, FL / September 2020 - September 2021</h4>
+            <div className='experience-details'>
+              <h4>Computer Programmer</h4>
+              <h5>Johnny on the Spot - Furniture Repair / Springhill, FL / September 2020 - September 2021</h5>
               <ul>
                 <li>Planned, Designed, Developed, and Maintained a contract management system that allowed this company to increase the outreach of its employees to individual clients.</li>
                 <li>Created a tool to get the company's capability statement distributed.</li>
                 <li>The CMS had a route customizer for day-to-day jobs based on employee locations.</li>
               </ul>
             </div>
-            <br></br>
-            <div className='experience'>
-              <h3>Freelance Developer</h3>
-              <h4>Kuscko Productions / Remote / June 2015 - August 2020</h4>
+            <div className='experience-details'>
+              <h4>Freelance Developer</h4>
+              <h5>Kuscko Productions / Remote / June 2015 - August 2020</h5>
               <ul>
                 <li>Provided custom scripting services, web application development and maintenance.</li>
                 <li>Created a resource management system that reduced any downtime an application was running on a server.</li>
               </ul>
             </div>
-            <br></br>
           </div>
-          <div id='skills' className='col-sm-6 col-md-6 col-lg-3'>
-            <div id='education' className='row'>
-              <h2>Education</h2>
-              <p>Pasco-Hernando State College: Associates in Science - Computer Programming and Analysis</p>
-            </div>
-            <br></br>
+        </div>
+        <div className='row'>
+          <div id='skills'>
             <h2>Skills</h2>
-            <ul>
-              <li>C#</li>
-              <li>ASP.NET</li>
-              <li>Entity Framework</li>
-              <li>SQL Server</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Bootstrap</li>
-              <li>Git</li>
-              <li>Visual Studio</li>
-              <li>Visual Studio Code</li>
-              <li>Agile</li>
-              <li>Test Driven Development</li>
-              <li>Unit Testing</li>
-              <li>Integration Testing</li>
-              <li>Automated Testing</li>
-              <li>Continuous Integration</li>
-              <li>Continuous Delivery</li>
-              <li>Continuous Deployment</li>
-            </ul>
+            <div id='skill-list'>
+              <div className='d-flex justify-content-start'>
+                <ul>
+                  <li>C#</li>
+                  <li>ASP.NET</li>
+                  <li>Entity Framework</li>
+                  <li>SQL Server</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                </ul>
+              </div>
+              <div className='d-flex justify-content-center'>
+                <ul>
+                  <li>React</li>
+                  <li>Bootstrap</li>
+                  <li>Git</li>
+                  <li>Visual Studio</li>
+                  <li>Visual Studio Code</li>
+                  <li>Agile</li>
+                  <li>Test Driven Development</li>
+                </ul>
+              </div>
+              <div className='d-flex justify-content-end'>
+                <ul>
+                  <li>Unit Testing</li>
+                  <li>Integration Testing</li>
+                  <li>Automated Testing</li>
+                  <li>Continuous Integration</li>
+                  <li>Continuous Delivery</li>
+                  <li>Continuous Deployment</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
